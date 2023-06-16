2fa82c03bcc8ac0031a2809814f4f4c388983a49
sejaljain2803@gmail.com <sejaljain2803@gmail.com>
2023-06-16 16:55:34 +0530

    integrate bootstrap for designing

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,19 +11,6 @@ class MoviesController < ApplicationController
       @movies = []
     end
   end
-  
-  def create
-    imdb_id = params[:imdb_id]
-    response = HTTParty.get("http://www.omdbapi.com/?i=#{imdb_id}&apikey=#{OMDB_API_KEY}")
-    movie_data = JSON.parse(response.body)
-
-    movie = Movie.new(imdb_id: imdb_id)
-    movie.save
-
-    # Process movie_data and store relevant information in your Movie model
-
-    redirect_to movie_path(movie)
-  end
 
   def show
     imdb_id = params[:id]
